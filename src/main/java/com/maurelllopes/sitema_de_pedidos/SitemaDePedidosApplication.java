e694a9463695ea69e155a0b544b6673ac5ae220f
Alexandre Maurell <maurelllopes@mac.com>
2021-05-18 15:11:30 -0300

    Projeto Criado

---
+++ src/main/java/com/maurelllopes/sitema_de_pedidos/SitemaDePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.maurelllopes.sitema_de_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemaDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemaDePedidosApplication.class, args);
+	}
+
+}
