d2d7b7265b7cabaa263be6d794a5d6e697d1f368
Alexandre Maurell <maurelllopes@mac.com>
2021-05-19 00:49:01 -0300

    Criando camadas REPOSITORY e SERVICE para CATEGORIA

---
+++ src/main/java/com/maurelllopes/sitema_de_pedidos/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.maurelllopes.sitema_de_pedidos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maurelllopes.sitema_de_pedidos.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+	
+	
+
+}
